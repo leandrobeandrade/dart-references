e710082f679212064300b3594c64d28ac7b2e077
Leandro B. Andrade <leandrobe.andrade@gmail.com>
2021-09-08 09:09:13 -0300

    Update and rename tipos to tipos.dart

---
+++ tipos-dados/tipos.dart
@@ -0,0 +1,22 @@
+// TIPOS
+ 
+// String  => declara uma variável como sendo do tipo string
+ 
+ 	String name = 'Fulano';
+ 	
+// int 	=> declara uma variável como sendo do tipo inteiro
+
+	int age = 20;
+	
+// double  => declara uma variável como sendo do tipo ponto flutuante
+
+	double value = 10.5;
+	
+// bool 	=> declara uma variável como sendo do tipo booleano
+
+	bool male = true;
+	
+// dynamic => declara uma variável como sendo de qualquer tipo
+
+  dynamic whatever = '10';
+  whatever = 10;
