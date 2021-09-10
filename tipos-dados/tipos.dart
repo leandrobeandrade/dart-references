7ef7467db65f5c81f4635ea2a6798018be67547c
Leandro B. Andrade <leandrobe.andrade@gmail.com>
2021-09-10 14:42:51 -0300

    Dart

--- tipos-dados/tipos.dart
+++ tipos-dados/tipos.dart
@@ -6,18 +6,18 @@ void main() {
   double peso = 64.30;
   var altura = 1.72;            		// tipo imutável
   dynamic linguagens = 'Dart';
-	final nums = 123;			// valor imutável
+  final nums = 123;				// valor imutável
 
-	print(nome);				// Fulano
+  print(nome);					// Fulano
   print(masculino);				// true
   print(idade);					// 18
   print(peso);					// 64.3
   print(altura);				// 1.72
   print(linguagens);				// Dart
-	print(nums);				// 123
 
   linguagens = ['Dart', 'TypeScript'];
   print(linguagens);				// ['Dart', 'TypeScript']
 	
-	nums = 456;				// ERRO 
+  print(nums);					// 123
+  nums = 456;					// ERRO 
 }
